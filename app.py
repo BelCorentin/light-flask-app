bc09831d37ba713597dd9b976e7d5ff09f81bca6
co <corentin.bel@protonmail.com>
2023-04-22 13:23:00 +0200

    host change

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def gpt4():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(debug=True, host="0.0.0.0", port=80)
+    # app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=80)
